d44364833c493f4301bb993eb3a9bbbbf33e28da
Zekun Wu <wu1326@xinu11.cs.purdue.edu>
2022-10-22 22:50:57 -0400

    1

--- lab6/system/bb_init.c
+++ lab6/system/bb_init.c
@@ -4,6 +4,15 @@
 #include <bb.h>
 #include <bb_prototypes.h>
 
+struct bbStruct bb;
+
 int32 bb_init() {
 	/* Your code here */
+
+	bb.bbListHead = 0;
+	bb.bbListTail = BB_SIZE - 1;
+	bb.bbPsem = semcreate(BB_SIZE);
+	bb.bbCsem = semcreate(0);
+	
+
 }
