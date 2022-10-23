27df1ab170144cb4883c97dd6f4727154900a1bd
Zekun Wu <wu1326@xinu11.cs.purdue.edu>
2022-10-22 22:50:39 -0400

    1

--- lab6/include/bb.h
+++ lab6/include/bb.h
@@ -1,1 +1,16 @@
 /* bb.h -- declare global variables used for the bounded buffer */
+#include <bb_prototypes.h>
+
+#define BB_UNINIT 0
+#define BB_INIT
+
+struct bbStruct {
+    byte bbList[BB_SIZE];
+    int32 bbListHead;
+    int32 bbListTail;
+    sid32 bbPsem;
+    sid32 bbCsem;
+    int32 bbState;
+};
+
+extern struct bbStruct bb;
