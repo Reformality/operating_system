bd4b6c08c3010109cc46e101854456287d3feaa9
Zekun Wu <zwu225@gmail.com>
2022-10-25 19:25:45 -0400

    init edit

--- lab6/system/bb_init.c
+++ lab6/system/bb_init.c
@@ -8,11 +8,14 @@ struct bbStruct bb;
 
 int32 bb_init() {
 	/* Your code here */
+	if (bb.bbState == BB_INIT || bb.bbState == BB_RUN || bb.bbState == BB_OUTONLY) {
+		return SYSERR;
+	}
 
 	bb.bbListHead = 0;
 	bb.bbListTail = BB_SIZE - 1;
 	bb.bbPsem = semcreate(BB_SIZE);
 	bb.bbCsem = semcreate(0);
-	
+	bb.bbState = BB_INIT;
 
 }
